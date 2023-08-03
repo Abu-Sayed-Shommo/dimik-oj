254b5e34ec1e24c81c5dc450ac4810d6e382239b
Abu Sayed Shommo <110298139+Abu-Sayed-Shommo@users.noreply.github.com>
2023-08-03 23:25:53 +0600

    24.cpp

--- 24.cpp
+++ 24.cpp
@@ -1,1 +1,25 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+#define Shommo ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+int main()
+{
+    Shommo;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        long long arr[n+10];
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        cout<<arr[0];
+        for(int i=2;i<n;i=i+2)
+        {
+            cout<<" "<<arr[i];
+        }
+        cout<<endl;
+    }
+}
